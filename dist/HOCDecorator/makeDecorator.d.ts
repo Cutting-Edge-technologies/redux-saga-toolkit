8e25b21ee1826ab044ccedfcb9034d3fa693099a
Kirill Penkin <kirill.penkin@doubleverify.com>
2023-12-20 21:58:58 +0400

    rebuild

--- dist/HOCDecorator/makeDecorator.d.ts
+++ dist/HOCDecorator/makeDecorator.d.ts
@@ -1,4 +1,4 @@
 import { StoryFn } from "@storybook/react";
 import { ExtendedStore } from "../types";
 import * as React from 'react';
-export declare const makeDecorator: (store: ExtendedStore<any>) => (Story: StoryFn<any>) => React.ReactNode;
+export declare const makeDecorator: (store: ExtendedStore<any>) => (Story: StoryFn<any>) => React.JSX.Element;
